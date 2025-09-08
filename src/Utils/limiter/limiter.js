d1f6eb7e47e454da732449dca02db551bb97c66e
lamia fekry <123817607+lamiafekry2003@users.noreply.github.com>
2025-09-08 22:21:21 +0300

    update rate limit

--- src/Utils/limiter/limiter.js
+++ src/Utils/limiter/limiter.js
@@ -1,8 +1,8 @@
 import { rateLimit } from 'express-rate-limit'
 export function limiter(){
     const limiter=rateLimit({
-        windowMs: 60 * 1000,   //1m
-        limit:3,
+        windowMs: 15 * 60 * 1000,   //1m
+        limit:100,
         message:{
           statusCode:429,
           message:'Too many requests from this IP,please try again'
